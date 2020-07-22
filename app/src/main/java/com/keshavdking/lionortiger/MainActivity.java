30a8ece704365b38e0959328c7c38a690554b5cf
keshavdking <keshupoddar@gmail.com>
2020-07-22 16:36:52 +0530

    changing and refactoring my codes for chking the winning sequences only

--- app/src/main/java/com/keshavdking/lionortiger/MainActivity.java
+++ app/src/main/java/com/keshavdking/lionortiger/MainActivity.java
@@ -2,13 +2,15 @@ package com.keshavdking.lionortiger;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 enum Player{
-    ONE,TWO
+    ONE,TWO,NONE
 }
 
 public class MainActivity extends AppCompatActivity {
@@ -17,76 +19,121 @@ public class MainActivity extends AppCompatActivity {
 
     Player playersChoices []=new Player[9];
 
-    int [] corners={0,2,6,8};
-//    int [][] arrayOfArrays = {{0,1,2},{3,4,4},{6,7,8}};
-    private int [] r1={0,1,2};
-    private int [] r2={3,4,5};
-    private int [] r3={6,7,8};
-    private int [] c1={0,3,6};
-    private int [] c2={1,4,7};
-    private int [] c3={2,5,8};
-    private int [] d1={0,4,8};
-    private int [] d2={2,4,6};
+    Button btnReset;
+    GridLayout gridView;
+
+
+    boolean gameOver=false;
+
+//    int [] corners={0,2,6,8};
+    //this line of code will be used to chk for winning cases
+    int [][] winingConditions = {{0,1,2},{3,4,4},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+//
+//    private int [] r1={0,1,2};
+//    private int [] r2={3,4,5};
+//    private int [] r3={6,7,8};
+//    private int [] c1={0,3,6};
+//    private int [] c2={1,4,7};
+//    private int [] c3={2,5,8};
+//    private int [] d1={0,4,8};
+//    private int [] d2={2,4,6};
 
 
 
 
 
     private Player currentPlayer=Player.ONE;
-    private Player clickedPlayer;
+//    private Player clickedPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i=0;i<playersChoices.length;i++){
+            playersChoices[i]=Player.NONE;
+        }
+        //        playersChoices[0]=Player.NONE;
+//        playersChoices[1]=Player.NONE;
+//        playersChoices[2]=Player.NONE;
+//        playersChoices[3]=Player.NONE;
+//        playersChoices[4]=Player.NONE;
+//        playersChoices[5]=Player.NONE;
+//        playersChoices[6]=Player.NONE;
+//        playersChoices[7]=Player.NONE;
+//        playersChoices[8]=Player.NONE;
+        btnReset=findViewById(R.id.btnReset);
+        gridView=findViewById(R.id.gridLayout);
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTheGame();
+            }
 
+
+        });
 
     }
+// this method is added directly to the image view in design part
+    public void imageIsTapped(View tappedImage) {
 
-    public void imageIsTapped(View tappedImage){
-
-        ImageView imageView = (ImageView) tappedImage;
-        imageView.setTranslationX(-2000);
-        final int tiIndex = Integer.parseInt(imageView.getTag().toString());
+        if (!gameOver) {
+            ImageView imageView = (ImageView) tappedImage;
+            imageView.setTranslationX(-2000);
+            final int tiIndex = Integer.parseInt(imageView.getTag().toString());
 //        Toast.makeText(this,tiIndex+"is tapped",Toast.LENGTH_SHORT).show();
 
+            //insert value in array
+            playersChoices[tiIndex] = currentPlayer;
 
-        //insert value in array
-        playersChoices[tiIndex]=currentPlayer;
-                //set image view
-        if (currentPlayer==Player.ONE){
-            imageView.setImageResource(R.drawable.lion);
-            clickedPlayer=currentPlayer;
-            currentPlayer=Player.TWO;
-        }else {
-            imageView.setImageResource(R.drawable.tiger);
-            clickedPlayer=currentPlayer;
-            currentPlayer=Player.ONE;
+            //set image view
+            if (currentPlayer == Player.ONE) {
+                imageView.setImageResource(R.drawable.lion);
+//            clickedPlayer=currentPlayer;
+                currentPlayer = Player.TWO;
+            } else {
+                imageView.setImageResource(R.drawable.tiger);
+//            clickedPlayer=currentPlayer;
+                currentPlayer = Player.ONE;
+            }
+
+            //does the animation
+            imageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+
+            //now we need to chk for conditions and other settings
+
+            for (int[] wincondition : winingConditions) {
+                if (playersChoices[wincondition[0]] == playersChoices[wincondition[1]] && playersChoices[wincondition[1]] == playersChoices[wincondition[2]] && playersChoices[wincondition[2]] != Player.NONE) {
+                    if (currentPlayer == Player.ONE) {
+                        Toast.makeText(this, "Tiger wins", Toast.LENGTH_SHORT).show();
+                    } else Toast.makeText(this, "Lion wins", Toast.LENGTH_SHORT).show();
+
+                    gameOver = true;
+                    btnReset.setVisibility(View.VISIBLE);
+
+                }
+            }
+
+
+//        boolean isTrue = contains(corners,tiIndex);
+//        //chk for winners
+//        //corner case
+//        if(isTrue){
+////            Toast.makeText(this,"corner is clicked",Toast.LENGTH_LONG).show();
+//            hrChk(tiIndex,clickedPlayer);
+//            vrChk(tiIndex,clickedPlayer);
+//            diaChk(tiIndex,clickedPlayer);
+//        }
+//        else if (tiIndex==4){
+//            hrChk(tiIndex,clickedPlayer);
+//            vrChk(tiIndex,clickedPlayer);
+//            diaChk(tiIndex,clickedPlayer);
+//        }else {
+//            hrChk(tiIndex,clickedPlayer);
+//            vrChk(tiIndex,clickedPlayer);
+//        }
+
+
         }
-
-        //does the animation
-        imageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
-
-        boolean isTrue = contains(corners,tiIndex);
-        //chk for winners
-        //corner case
-        if(isTrue){
-//            Toast.makeText(this,"corner is clicked",Toast.LENGTH_LONG).show();
-            hrChk(tiIndex,clickedPlayer);
-            vrChk(tiIndex,clickedPlayer);
-            diaChk(tiIndex,clickedPlayer);
-        }
-        else if (tiIndex==4){
-            hrChk(tiIndex,clickedPlayer);
-            vrChk(tiIndex,clickedPlayer);
-            diaChk(tiIndex,clickedPlayer);
-        }else {
-            hrChk(tiIndex,clickedPlayer);
-            vrChk(tiIndex,clickedPlayer);
-        }
-
-
-    }
-
+/* this was my first case
     //horizontal row chk
     private void hrChk(int i,Player p){
 
@@ -202,5 +249,33 @@ public class MainActivity extends AppCompatActivity {
     private void youwon(){
         Toast.makeText(this,"you won",Toast.LENGTH_LONG).show();
     }
+*/
+    }
 
+    private void resetTheGame() {
+
+        for (int i=0;i<playersChoices.length;i++){
+            playersChoices[i]=Player.NONE;
+        }
+        //        playersChoices[0]=Player.NONE;
+//        playersChoices[1]=Player.NONE;
+//        playersChoices[2]=Player.NONE;
+//        playersChoices[3]=Player.NONE;
+//        playersChoices[4]=Player.NONE;
+//        playersChoices[5]=Player.NONE;
+//        playersChoices[6]=Player.NONE;
+//        playersChoices[7]=Player.NONE;
+//        playersChoices[8]=Player.NONE;
+
+        for (int i=0;i<gridView.getChildCount();i++){
+            ImageView imageView = (ImageView) gridView.getChildAt(i);
+            imageView.setImageDrawable(null);
+            imageView.setAlpha(0.2f);
+        }
+
+        currentPlayer=Player.ONE;
+        gameOver=false;
+        btnReset.setVisibility(View.GONE);
+
+    }
 }
